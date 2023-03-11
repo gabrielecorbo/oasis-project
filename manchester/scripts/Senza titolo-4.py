cc6d9d33af4af445b37ffc0d246b82071d8b5af7
Gabriele Corbo <gabricorbo99@gmail.com>
2023-03-11 15:31:53 +0100

    esagonalizzazione

--- manchester/scripts/Senza titolo-4.py
+++
@@ -1,8 +0,0 @@
-#base = df_roads_exc_mtrwy.plot(figsize=(12, 8), color='deepskyblue', lw=0.4, zorder=0)  # Zorder controls the layering of the charts with
-#df_roads_exc_mtrwy.plot(ax=base)
-
-traffic_points_gdf = traffic_points_gdf.drop(labels=['year','road_name','road_type'
-                                                     ,'link_length_km','count_point_id',
-                                                     'easting','northing','latitude','longitude'], axis=1)
-traffic_points_gdf.plot( x='horizontal', y='vertical', c='cars_and_taxis', cmap='viridis', kind='scatter', s=7, zorder=10)
-plt.show()
