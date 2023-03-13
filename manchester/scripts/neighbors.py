2514926b36c062c62d2026a89f0f3a048c07fbc0
Gabriele Corbo <gabricorbo99@gmail.com>
2023-03-13 16:43:36 +0100

    vicinilontani

--- manchester/scripts/neighbors.py
+++ manchester/scripts/neighbors.py
@@ -31,9 +31,47 @@ def neighbors(rows,colums,index):
         if (i_inx-1)>=0 and (j_inx-1)>=0:
             v.append(mat[i_inx-1,j_inx-1])
     print(v)
-    return v
+
+    ##### second level
+    v2=[]
+    if (i_inx-2)>=0:
+        v2.append(mat[i_inx-2,j_inx])
+    if (i_inx+2)<rows:
+        v2.append(mat[i_inx+2,j_inx])
+    if (j_inx-2)>=0:
+        v2.append(mat[i_inx,j_inx-2])
+    if (j_inx+2)<colums:
+        v2.append(mat[i_inx,j_inx+2])
+    if (i_inx+2)<rows and (j_inx+1)<colums:
+        v2.append(mat[i_inx+2,j_inx+1])
+    if (i_inx+2)<rows and (j_inx-1)>=0:
+        v2.append(mat[i_inx+2,j_inx-1])
+    if (i_inx-2)>=0 and (j_inx+1)<colums:
+        v2.append(mat[i_inx-2,j_inx+1])
+    if (i_inx-2)>=0 and (j_inx-1)>=0:
+        v2.append(mat[i_inx-2,j_inx-1])
+    if (rows-i_inx)%2==0:   #seconda quarta sesta ecc riga dal basso
+        if (i_inx+1)<rows and (j_inx-1)>=0:
+            v2.append(mat[i_inx+1,j_inx-1])
+        if (i_inx-1)>=0 and (j_inx-1)>=0:
+            v2.append(mat[i_inx-1,j_inx-1])
+        if (i_inx+1)<rows and (j_inx+2)<colums:
+            v2.append(mat[i_inx+1,j_inx+2])
+        if (i_inx-1)>=0 and (j_inx+2)<colums:
+            v2.append(mat[i_inx-1,j_inx+2])
+    else:
+        if (i_inx+1)<rows and (j_inx-2)>=0:
+            v2.append(mat[i_inx+1,j_inx-2])
+        if (i_inx-1)>=0 and (j_inx-2)>=0:
+            v2.append(mat[i_inx-1,j_inx-2])
+        if (i_inx+1)<rows and (j_inx+1)<colums:
+            v2.append(mat[i_inx+1,j_inx+1])
+        if (i_inx-1)>=0 and (j_inx+1)<colums:
+            v2.append(mat[i_inx-1,j_inx+1])
+    print(v2)
+    return v,v2
 
 
-neighbors(6,6,8)
+neighbors(6,6,0)
     
      
